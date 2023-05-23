9f9be97f02f40c4ac728784d6ca9799a8cd867c0
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-22 22:47:40 -0500

    Add problem solving to challenge3

---
+++ challenge3.rb
@@ -0,0 +1,12 @@
+# 1. Write a method or function that removes all instances of the letter <strong>s</strong> in a string. 
+# The method or function should accept a string as an argument and return the same string with every instance of 
+# the letter <strong>s</strong> removed.
+
+# Goal: I need to write a method tthat accepts a string as an argument but then returns that string without the letter s.
+# Data typs: methods, strings,
+# Steps:
+# 1. Create a method with a string parameter 
+# 2. def the method to take out the letter s
+# 3. Use puts to add an argument to the method to test effectiveness
+# Research needed - look for a way to take out a specific element in a string
+
