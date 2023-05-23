e41ba89a6f826ebb795441f95eeef23cb2d854c3
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-22 22:58:58 -0500

    Begin challenge4

---
+++ challenge4.rb
@@ -0,0 +1,6 @@
+Start with an array of travel destinations. Print every travel destination in alphabetical order embedded 
+in a sentence using string interpolation. For example, if the destination is "New York City", print something like 
+    "The next place I want to visit is New York City!"
+
+Goal: The goal of this challenge is to create an array with strings of travel destinations. I will need to print them in alphabetical
+order and interpolate the words within a sentence
